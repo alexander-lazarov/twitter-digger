4bbc9f736f6ee3693a92fa13616730127f385e1f
Alexander Lazarov <alexander.lazaroff@gmail.com>
2016-06-28 19:03:43 +0300

    Refactor code

--- twitter-digger/twitter-digger.py
+++ twitter-digger/twitter-digger.py
@@ -28,23 +28,21 @@ args_parser.add_argument(
 args = args_parser.parse_args()
 
 
-with open(args.user_list) as inp:
-    reader = csv.reader
+class Downloader():
+    def __init__(self, api, session):
+        self.api = api
+        self.session = session
 
-    api = api_factory()
-    session = session_factory()
-
-    reader = csv.DictReader(inp)
-    for row in reader:
-        print('Querying user: {}'.format(row['user']))
+    def handle_user(self, username):
+        print('Querying user: {}'.format(username))
 
         try:
-            user_data = api.get_user(row['user'])
+            user_data = self.api.get_user(username)
         except tweepy.error.TweepError:
-            print('User {} not found'.format(row['user']))
-            continue
+            print('User {} not found'.format(username))
+            return
 
-        user = session.query(User).get(user_data.id)
+        user = self.session.query(User).get(user_data.id)
 
         if user is None:
             user = User()
@@ -54,9 +52,12 @@ with open(args.user_list) as inp:
         user.friends_count = user_data.friends_count
         user.followers_count = user_data.followers_count
 
-        tweets = api.user_timeline(row['user'], count=200)
+        self._handle_tweets(user)
+
+    def _handle_tweets(self, user):
+        tweets = self.api.user_timeline(row['user'], count=200)
         for tweet_data in tweets:
-            tweet = session.query(Tweet).get(tweet_data.id)
+            tweet = self.session.query(Tweet).get(tweet_data.id)
 
             if tweet is None:
                 tweet = Tweet()
@@ -66,10 +67,23 @@ with open(args.user_list) as inp:
             tweet.retweet_count = tweet_data.retweet_count
             tweet.user = user
 
-            session.add(tweet)
+            self.session.add(tweet)
 
         print('Downloaded {} tweets for user {}'.format(
-            len(tweets), row['user']))
+            len(tweets), user.screen_name))
 
-        session.add(user)
-        session.commit()
+        self.session.add(user)
+        self.session.commit()
+
+
+with open(args.user_list) as inp:
+    reader = csv.reader
+
+    api = api_factory()
+    session = session_factory()
+
+    downloader = Downloader(api, session)
+
+    reader = csv.DictReader(inp)
+    for row in reader:
+        downloader.handle_user(row['user'])
